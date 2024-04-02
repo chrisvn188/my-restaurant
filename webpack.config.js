506425c06131d75eee8e8dd3ac512ea6f3c420e8
chrisvn188 <chrisbui.dev188@gmail.com>
2024-04-02 09:18:13 -0400

    Create project basic structure

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            head: true,
+            scriptLoading: 'defer',
+        }),
+    ],
+}
